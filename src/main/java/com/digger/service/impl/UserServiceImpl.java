a92a03f77a3f3cdb2ca389e9191fcfb6c68d93b3
Jhin1 <867493059@qq.com>
2018-09-07 18:00:24 +0800

    Shiro框架

--- src/main/java/com/digger/service/impl/UserServiceImpl.java
+++ src/main/java/com/digger/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.digger.common.ServerResponse;
 import com.digger.dao.UserMapper;
 import com.digger.pojo.User;
 import com.digger.service.UserService;
+import com.digger.utils.MD5Util;
 
 @Service("userService")
 public class UserServiceImpl implements UserService{
@@ -25,11 +26,14 @@ public class UserServiceImpl implements UserService{
 		if(resultCount == 0 ){
             return ServerResponse.createByErrorMessage("用户名不存在");
         }
-		User user  = userMapper.selectLogin(username,password);
+		String md5Password = MD5Util.MD5EncodeUtf8(password);
+		System.out.println(md5Password);
+        User user  = userMapper.selectLogin(username,md5Password);
 		if(user == null){
 	        return ServerResponse.createByErrorMessage("密码错误");
 	    }
-		return ServerResponse.createBySuccess("登录成功",user);
+		user.setPassword(org.apache.commons.lang3.StringUtils.EMPTY);
+        return ServerResponse.createBySuccess("登录成功",user);
 	}
 
 }
