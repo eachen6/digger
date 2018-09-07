22c4dd781ad163c940f35af87508b00e1d97a930
Jhin1 <867493059@qq.com>
2018-09-07 17:22:37 +0800

    登陆

--- src/main/java/com/digger/dao/UserMapper.java
+++ src/main/java/com/digger/dao/UserMapper.java
@@ -3,6 +3,7 @@ package com.digger.dao;
 import org.apache.ibatis.annotations.Param;
 
 import com.digger.pojo.User;
+import com.digger.common.ServerResponse;
 
 public interface UserMapper {
     int deleteByPrimaryKey(Integer id);
@@ -16,6 +17,11 @@ public interface UserMapper {
     int updateByPrimaryKeySelective(User record);
 
     int updateByPrimaryKey(User record);
+
+	int checkUsername(String username);
+
+	User selectLogin(@Param("username")String username,@Param("password")String password);
+
     
-    User todologin(@Param("username")String username);
+    
 }
--- src/main/java/com/digger/service/UserService.java
+++ src/main/java/com/digger/service/UserService.java
@@ -3,12 +3,13 @@ package com.digger.service;
 import org.springframework.stereotype.Service;
 
 import com.digger.pojo.User;
+import com.digger.common.ServerResponse;
 
 
 public interface UserService {
 	
 	public int register(User user);
 	
-	public User todologin(String username);
+	ServerResponse<User> login(String username, String password);
 	
 }
