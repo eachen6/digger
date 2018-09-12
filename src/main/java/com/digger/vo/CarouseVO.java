129e54bced7270c4cb58271ecd18bf5f43122a4e
Jhin1 <867493059@qq.com>
2018-09-12 10:42:13 +0800

    VO屬性補充

--- src/main/java/com/digger/vo/CarouseVO.java
+++ src/main/java/com/digger/vo/CarouseVO.java
@@ -1,8 +1,17 @@
 package com.digger.vo;
 
+import java.util.Date;
+
+//獲取熱銷輪播圖
 public class CarouseVO {
 	private int id;
 	private String carouseurl;
+	private String name;
+	private String price;
+	private String discount;
+	private Date shelftime;
+	private String coverurl;
+	private Date deadline;
 	public int getId() {
 		return id;
 	}
@@ -15,4 +24,41 @@ public class CarouseVO {
 	public void setCarouseurl(String carouseurl) {
 		this.carouseurl = carouseurl;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getDiscount() {
+		return discount;
+	}
+	public void setDiscount(String discount) {
+		this.discount = discount;
+	}
+	public Date getShelftime() {
+		return shelftime;
+	}
+	public void setShelftime(Date shelftime) {
+		this.shelftime = shelftime;
+	}
+	public String getCoverurl() {
+		return coverurl;
+	}
+	public void setCoverurl(String coverurl) {
+		this.coverurl = coverurl;
+	}
+	public Date getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+
 }
